c2d50da711c5e18173ceafa64eca6da76ee7d966
马明帅 <mamingshuai1@huawei.com>
2021-06-22 02:57:32 +0000

    update post-sync.py.

--- post-sync.py
+++ post-sync.py
@@ -1,7 +1,7 @@
 import os, sys
 
 def main(**kwargs):
-    print("Execute prebuild_download", file=sys.stdout)
-    os.system("prebuild_download")
+    #print("Execute prebuild_download", file=sys.stdout)
+    #os.system("prebuild_download")
     print("Execute repo forall git lfs pull", file=sys.stdout)
     os.system("~/oschina/repo/repo forall -c git lfs pull")
